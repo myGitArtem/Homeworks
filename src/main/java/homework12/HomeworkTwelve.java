50529e7c0626c9cde2e311c56591c3445e62f786
UserS <artembezdugan@gmail.com>
2022-03-30 12:16:09 +0300

    Hw12

--- src/main/java/homework12/HomeworkTwelve.java
+++ src/main/java/homework12/HomeworkTwelve.java
@@ -1,6 +1,6 @@
 package homework12;
 
-public class HomeworkEleven {
+public class HomeworkTwelve {
     public static void main(String[] args) {
         System.out.println(PersonFactory.createPersons());
     }
