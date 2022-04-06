01655d3e022e5194bf2cd1b630c89fed4571fcce
UserS <artembezdugan@gmail.com>
2022-04-06 22:39:43 +0300

    Hw13

---
+++ src/main/java/homework13/Fance.java
@@ -0,0 +1,13 @@
+package homework13;
+
+public class Fance extends Units{
+
+    public Fance(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("This is fance");
+    }
+}
